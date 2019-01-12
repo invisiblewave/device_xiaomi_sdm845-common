8ec877f996977abc8cb4fea3216de7f814aef69d
AndroPlus <mail@andro.plus>
2019-01-12 10:51:41 +0900

    TEMP: Disable IFAA for now

--- sdm845.mk
+++ sdm845.mk
@@ -48,11 +48,11 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0
 
 # IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
+#PRODUCT_PACKAGES += \
+#    org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # IMS
 PRODUCT_PACKAGES += \
